fb1d5dea9a31903005d6401e1d052a4aa723d8d7
luyuanfan <luyuanfan1@gmail.com>
2025-06-25 16:42:29 -0400

    Fixed custom element troubles; added local fonts

--- js/footer.js
+++ js/footer.js
@@ -4,13 +4,11 @@ class FooterComponent extends HTMLElement {
   }
 
   connectedCallback() {
+    console.log("Custom move-handling logic here.");
     this.innerHTML = `
-      <footer-component>
-        <span>[email: <a href="mailto:lyspfan@gmail.com">lyspfan@gmail.com</a>]</span>
-        <span>[<a href="https://www.reddit.com/r/TVTooHigh/comments/1anljpv/wughh_guys_do_you_think_the_tv_is_too_high_eughh/">reddit</a>]</span>
-        <span>[<a href="about.html#legal-name" target="_blank">gamer stats</a>]</span>
-        <span>[<a href="trips.html" target="_blank">trips</a>]</span>
-      </footer-component>
+      <span>[email: <a href="mailto:lyspfan@gmail.com">lyspfan@gmail.com</a>]</span>
+      <span>[<a href="https://www.reddit.com/r/TVTooHigh/comments/1anljpv/wughh_guys_do_you_think_the_tv_is_too_high_eughh/">reddit</a>]</span>
+      <span>[<a href="trips.html" target="_blank">trips</a>]</span>
     `;
   }
 }
