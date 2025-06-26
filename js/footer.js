2ffc13032a4da59c70e40a3bc3fc2cf39ce9a261
luyuanfan <luyuanfan1@gmail.com>
2025-06-26 00:05:31 -0400

    Fixed footer script target

--- js/footer.js
+++ js/footer.js
@@ -6,9 +6,9 @@ class FooterComponent extends HTMLElement {
   connectedCallback() {
     console.log("Custom move-handling logic here.");
     this.innerHTML = `
-      <span>[<a href="about.html" target="_blank">about me</a>]</span>
-      <span>[<a href="https://www.reddit.com/r/TVTooHigh/comments/1anljpv/wughh_guys_do_you_think_the_tv_is_too_high_eughh/">reddit</a>]</span>
-      <span>[<a href="trips.html" target="_blank">trips</a>]</span>
+      <span>[<a href="about.html">about me</a>]</span>
+      <span>[<a href="https://www.reddit.com/r/TVTooHigh/comments/1anljpv/wughh_guys_do_you_think_the_tv_is_too_high_eughh/" target="_blank">reddit</a>]</span>
+      <span>[<a href="trips.html">trips</a>]</span>
       <span>[email: <a href="mailto:lyspfan@gmail.com">lyspfan@gmail.com</a>]</span>
     `;
   }
