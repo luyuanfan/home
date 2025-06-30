6a4b29657e3d67bdddf7ea44ab342173fc1f7105
luyuanfan <luyuanfan1@gmail.com>
2025-06-30 13:01:51 -0400

    Updated footer and bad webkit reference?

--- js/footer.js
+++ js/footer.js
@@ -7,11 +7,12 @@ class FooterComponent extends HTMLElement {
     console.log("Custom move-handling logic here.");
     this.innerHTML = `
       <span>[<a href="about.html">about me</a>]</span>
-      <span>[<a href="https://www.reddit.com/r/TVTooHigh/comments/1anljpv/wughh_guys_do_you_think_the_tv_is_too_high_eughh/" target="_blank">reddit</a>]</span>
       <span>[<a href="trips.html">trips</a>]</span>
-      <span>[email: <a href="mailto:lyspfan@gmail.com">lyspfan@gmail.com</a>]</span>
       <span>[<a href="misc.html">misc</a>]</span>
-    `;
+      <span>[<a href="https://www.reddit.com/r/TVTooHigh/comments/1anljpv/wughh_guys_do_you_think_the_tv_is_too_high_eughh/" target="_blank">reddit</a>]</span>
+      <br>
+      <span>[email: <a href="mailto:lyspfan@gmail.com">lyspfan@gmail.com</a>]</span>
+      `;
   }
 }
 
